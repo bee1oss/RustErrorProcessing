9dbda4debea907ed562171690fa4630d0db8d44d
bee1oss <46090840+bee1oss@users.noreply.github.com>
2023-07-17 14:01:44 +0300

    panis result
    trainee panic and result

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
 fn main() {
-    println!("Hello, world!");
+    hwresult();
+}
+
+fn metunwrap(){
+    let path = "history.json";
+    let f = File::open(path).unwrap();//match ile yaptigimiz hata yakalama islemini daha hazili ve kolay yolla yapilmaktadir.
+    
+}
+
+fn hwresult() {
+    let path = "history.json";
+    let f = File::open(path);
+
+    let f = match f {
+        Ok(file) => file,
+        Err(e) => match e.kind() {
+            ErrorKind::NotFound => match File::create(path) {
+                Ok(file) => file,
+                Err(e) => panic!("Error creating file :{e:?}"),
+            },
+            other => panic!("ERROR OCCURED: {:?}", other)
+        },
+    };
 }
